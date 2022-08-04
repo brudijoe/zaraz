55903e82e3bffb2b048b1ab6e00a650d9fffe091
Ingo Speckens <doningo@ymail.com>
2022-08-04 20:21:04 +0200

    initial_commit

---
+++ zaraz-server/src/test/java/com/brudijoe/zarazserver/ZarazServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brudijoe.zarazserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarazServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
